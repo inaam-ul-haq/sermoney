e0893abad3c7b3fed7dfc3d399a8555c04ed7ce0
asmaafraz <asmakanwalsatti@gmail.com>
2024-07-01 14:34:31 +0500

    "Changes Done"

--- resources/views/userdashboard/addresses.blade.php
+++ resources/views/userdashboard/addresses.blade.php
@@ -17,56 +17,57 @@
                                 <i class="bx bx-menu bx-md"></i>
                             </a>
                         </div>
-
                         <div class="row my-2">
                             <div class="col-lg-9 table-content p-3 mt-4 rounded">
-                               @php
-   $user= Auth::user();
-@endphp
-  @if ($user->status === 'accepted')
-                                @if ($warehouses->isEmpty())
-                                    <p>No warehouses found.</p>
-                                @else
-                                    <div class="row">
-                                        <!-- Loop through warehouses and add data-id attribute to the Update button -->
-                                        @foreach ($warehouses as $warehouse)
-                                            <div class="col-md-12 col-12 mb-3">
-                                                <div class="card">
-                                                    <div class="card-header d-flex justify-content-between">
-                                                        <div class="">
-                                                            <h5>{{ $warehouse->name }}</h5>
+                                @php
+                                    $user = Auth::user();
+                                @endphp
+                                @if ($user->status === 'accepted')
+                                    @if ($warehouses->isEmpty())
+                                        <p>No warehouses found.</p>
+                                    @else
+                                        <div class="d-flex flex-wrap">
+                                            <!-- Loop through warehouses and add data-id attribute to the Update button -->
+                                            @foreach ($warehouses as $warehouse)
+                                                <div class="col-md-6 col-12 mb-3">
+                                                    <div class="card">
+                                                        <div class="card-header d-flex justify-content-between">
+                                                            <div>
+                                                                <h5>{{ $warehouse->name }}</h5>
+                                                            </div>
+                                                            <div>
+                                                                <button type="button" class="btn btn-sm btn-success"
+                                                                    onclick="copyToClipboard('address-{{ $warehouse->id }}')">
+                                                                    Copy
+                                                                </button>
+                                                            </div>
                                                         </div>
-                                                        <div class="">
-                                                            <button type="button" class="btn btn-sm btn-success" onclick="copyToClipboard('address-{{ $warehouse->id }}')">
-                                                                Copy
-                                                            </button>
-                                                        </div>
-                                                    </div>
-                                                    <div class="card-body">
-                                                        @if ($warehouse->addresses->isEmpty())
-                                                            <p>No addresses found for this warehouse.</p>
-                                                        @else
-                                                            <div id="address-{{ $warehouse->id }}" class="d-none">
-                                                                @foreach ($warehouse->addresses as $address)
-                                                                    Country: {{ $address->country }},
-                                                                    State: {{ $address->state }},
-                                                                    City: {{ $address->city }},
-                                                                    Street: {{ $address->street }},
-                                                                    Postal Code: {{ $address->postal_code }}
-                                                                    <br>
-                                                                @endforeach
+                                                        <div class="card-body">
+                                                            @if ($warehouse->addresses->isEmpty())
+                                                                <p>No addresses found for this warehouse.</p>
+                                                            @else
+                                                                <div id="address-{{ $warehouse->id }}" class="d-none">
+                                                                    @foreach ($warehouse->addresses as $address)
+                                                                        Country: {{ $address->country }},
+                                                                        State: {{ $address->state }},
+                                                                        City: {{ $address->city }},
+                                                                        Street: {{ $address->street }},
+                                                                        Postal Code: {{ $address->postal_code }}
+                                                                        <br>
+                                                                    @endforeach
                                                                 </div>
                                                                 <table class="table table-bordered">
                                                                     <tbody>
                                                                         <tr>
                                                                             <td class="fw-bold">Type</td>
-                                                                            <td>  {{ $user->name }}
-                                                                            @if ($warehouse->type == 'AIR')
-                                                                                SIRM
-                                                                            @elseif ($warehouse->type == 'MARITIME')
-                                                                                SIRM MAR
-                                                                            @endif
-                                                                            {{ $registration->pobox }}</td>
+                                                                            <td>{{ $user->name }}
+                                                                                @if ($warehouse->type == 'AIR')
+                                                                                    SIRM
+                                                                                @elseif ($warehouse->type == 'MARITIME')
+                                                                                    SIRM MAR
+                                                                                @endif
+                                                                                {{ $registration->pobox }}
+                                                                            </td>
                                                                         </tr>
                                                                         @foreach ($warehouse->addresses as $address)
                                                                             <tr>
@@ -104,11 +105,9 @@
                                         </div>
                                     @endif
                                 @else
-                                       <div class="card bg-warning text-dark"
-                                        style="    border-radius: 0.375rem !important;
-    top: 12rem;
-    margin-left: 25rem;width: 26rem;">
-                                         <div class="card-body">
+                                    <div class="card bg-warning text-dark"
+                                        style="border-radius: 0.375rem !important; top: 12rem; margin-left: 25rem; width: 26rem;">
+                                        <div class="card-body">
                                             @if ($user->status === 'pending')
                                                 <p class="card-text">Your profile is pending. Please wait for approval.</p>
                                             @elseif ($user->status === 'rejected')
@@ -122,6 +121,114 @@
                                 @endif
                             </div>
                         </div>
+
+
+                        {{-- <div class="row my-2">
+                            <div class="col-lg-9 table-content p-3 mt-4 rounded">
+                                @php
+                                    $user = Auth::user();
+                                @endphp
+                                @if ($user->status === 'accepted')
+                                    @if ($warehouses->isEmpty())
+                                        <p>No warehouses found.</p>
+                                    @else
+                                        <div class="row">
+                                            <!-- Loop through warehouses and add data-id attribute to the Update button -->
+                                            @foreach ($warehouses as $warehouse)
+                                                <div class="col-md-12 col-12 mb-3">
+                                                    <div class="card">
+                                                        <div class="card-header d-flex justify-content-between">
+                                                            <div class="">
+                                                                <h5>{{ $warehouse->name }}</h5>
+                                                            </div>
+                                                            <div class="">
+                                                                <button type="button" class="btn btn-sm btn-success"
+                                                                    onclick="copyToClipboard('address-{{ $warehouse->id }}')">
+                                                                    Copy
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            @if ($warehouse->addresses->isEmpty())
+                                                                <p>No addresses found for this warehouse.</p>
+                                                            @else
+                                                                <div id="address-{{ $warehouse->id }}" class="d-none">
+                                                                    @foreach ($warehouse->addresses as $address)
+                                                                        Country: {{ $address->country }},
+                                                                        State: {{ $address->state }},
+                                                                        City: {{ $address->city }},
+                                                                        Street: {{ $address->street }},
+                                                                        Postal Code: {{ $address->postal_code }}
+                                                                        <br>
+                                                                    @endforeach
+                                                                </div>
+                                                                <table class="table table-bordered">
+                                                                    <tbody>
+                                                                        <tr>
+                                                                            <td class="fw-bold">Type</td>
+                                                                            <td> {{ $user->name }}
+                                                                                @if ($warehouse->type == 'AIR')
+                                                                                    SIRM
+                                                                                @elseif ($warehouse->type == 'MARITIME')
+                                                                                    SIRM MAR
+                                                                                @endif
+                                                                                {{ $registration->pobox }}
+                                                                            </td>
+                                                                        </tr>
+                                                                        @foreach ($warehouse->addresses as $address)
+                                                                            <tr>
+                                                                                <td class="fw-bold">Country</td>
+                                                                                <td>{{ $address->country }}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td class="fw-bold">State</td>
+                                                                                <td>{{ $address->state }}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td class="fw-bold">City</td>
+                                                                                <td>{{ $address->city }}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td class="fw-bold">Street No</td>
+                                                                                <td>{{ $address->street }}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td class="fw-bold">Postal Code</td>
+                                                                                <td>{{ $address->postal_code }}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td class="fw-bold">Telefono</td>
+                                                                                <td>{{ $address->Telefono }}</td>
+                                                                            </tr>
+                                                                        @endforeach
+                                                                    </tbody>
+                                                                </table>
+                                                            @endif
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    @endif
+                                @else
+                                    <div class="card bg-warning text-dark"
+                                        style="    border-radius: 0.375rem !important;
+    top: 12rem;
+    margin-left: 25rem;width: 26rem;">
+                                        <div class="card-body">
+                                            @if ($user->status === 'pending')
+                                                <p class="card-text">Your profile is pending. Please wait for approval.</p>
+                                            @elseif ($user->status === 'rejected')
+                                                <p class="card-text">Your profile has been rejected. Please contact support
+                                                    for more information.</p>
+                                            @else
+                                                <p class="card-text">You are not authorized to view this content.</p>
+                                            @endif
+                                        </div>
+                                    </div>
+                                @endif
+                            </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
